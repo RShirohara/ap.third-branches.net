83269feea265bdb39fab6a19a83513d9a3a9891d
Ray Shirohara <RShirohara@proton.me>
2024-09-16 16:31:26 +0900

    chore(deps): bump container `cloudflare/cloudflared` version to `2024.9.1` (#79)
    Bump container `cloudflare/cloudflared` version to `2024.9.1`.

--- terraform/modules/aws-lightsail-container/main.tf
+++ terraform/modules/aws-lightsail-container/main.tf
@@ -48,7 +48,7 @@ resource "aws_lightsail_container_service_deployment_version" "gotosocial_contai
 
   container {
     container_name = "tunnel"
-    image          = "cloudflare/cloudflared:2024.8.3"
+    image          = "cloudflare/cloudflared:2024.9.1"
 
     command = ["tunnel", "run"]
 
