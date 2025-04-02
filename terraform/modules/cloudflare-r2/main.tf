f46d741b9894adb5e65abb45339486fe017f4086
Ray Shirohara <RShirohara@proton.me>
2025-04-03 00:26:59 +0900

    fix: resolve `cloudflare/cloudflare` v5.x migration

--- terraform/modules/cloudflare-r2/main.tf
+++ terraform/modules/cloudflare-r2/main.tf
@@ -20,26 +20,52 @@ resource "cloudflare_r2_bucket" "gotosocial_media" {
 resource "cloudflare_api_token" "gotosocial_media" {
   name = "gotosocial-media"
 
-  policy {
-    permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Read"],
-      data.cloudflare_api_token_permission_groups.all.r2["Workers R2 Storage Bucket Item Write"]
-    ]
-
-    resources = {
-      "com.cloudflare.edge.r2.bucket.${var.account_id}_default_${cloudflare_r2_bucket.gotosocial_media.id}" = "*"
+  # WORKAROUND: <https://github.com/cloudflare/terraform-provider-cloudflare/issues/5062#issuecomment-2749703161>
+  policies = [
+    {
+      effect = "allow"
+      permission_groups = [
+        {
+          id = element(
+            data.cloudflare_api_token_permission_groups_list.all.result,
+            index(
+              data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+              "Workers R2 Storage Bucket Item Read"
+            )
+          ).id,
+        },
+        {
+          id = element(
+            data.cloudflare_api_token_permission_groups_list.all.result,
+            index(
+              data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+              "Workers R2 Storage Bucket Item Write"
+            )
+          ).id
+        },
+      ]
+      resources = {
+        "com.cloudflare.edge.r2.bucket.${var.account_id}_default_${cloudflare_r2_bucket.gotosocial_media.id}" = "*"
+      }
+    },
+    {
+      effect = "allow"
+      permission_groups = [
+        {
+          id = element(
+            data.cloudflare_api_token_permission_groups_list.all.result,
+            index(
+              data.cloudflare_api_token_permission_groups_list.all.result.*.name,
+              "Workers R2 Storage Read"
+            )
+          ).id
+        }
+      ]
+      resources = {
+        "com.cloudflare.api.account.${var.account_id}" = "*"
+      }
     }
-  }
-
-  policy {
-    permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.account["Workers R2 Storage Read"]
-    ]
-
-    resources = {
-      "com.cloudflare.api.account.${var.account_id}" = "*"
-    }
-  }
+  ]
 }
 
-data "cloudflare_api_token_permission_groups" "all" {}
+data "cloudflare_api_token_permission_groups_list" "all" {}
